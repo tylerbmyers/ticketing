23a9888d8a291860676499f569b6c0d891979c5d
Tyler Myers <tyler.b.myers2@gmail.com>
2020-11-02 20:22:24 -0500

    fixes for digital ocean

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,14 +5,13 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers,
+      baseURL: 'http://www.ticketing-app-prod.digital',
+      headers: req.headers
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseURL: '/',
+      baseURL: '/'
     });
   }
 };
